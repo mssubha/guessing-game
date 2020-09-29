6329188efc7a46380558d0bb392d7d6ae7b35da5
vagrant <vagrant@vagrant.vm>
2020-09-29 23:40:25 +0000

    Working solution to the problem presented.

--- game.py
+++ game.py
@@ -1,3 +1,24 @@
 """A number-guessing game."""
 
 # Put your code here
+from random import randint
+
+name = input('Greetings, what is your name? : ')
+print(f'Hi, {name}')
+
+target_number = randint(1,100)
+
+guess = int(input(f'{name}, please choose a number between 1 and 100. : '))
+counter = 0
+
+
+while not guess == target_number:
+    counter += 1
+    if guess < target_number:
+        guess = int(input('We\'re sorry, your guess is too low. Please try again. : '))
+    elif guess > target_number:
+        guess = int(input('We\'re sorry, your guess is too high. Please try again. : '))
+
+
+print(f'Congratulations {name}, {guess} is indeed the target.')
+print(f'You completed the game in {counter} tries.')
